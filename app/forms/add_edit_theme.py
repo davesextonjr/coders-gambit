1ca12b04931425e8d3ddbb28b67eb0298d10cdef
David Sexton Jr <davesextonjr@gmail.com>
2023-01-04 19:54:19 -0500

    add theme model and form

---
+++ app/forms/add_edit_theme.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, URLField
+from wtforms.validators import DataRequired, Length
+
+class AddEditThemeForm(FlaskForm):
+    user_id = IntegerField('user_id', validators=[DataRequired(message='Must be a logged in user')])
+    theme_name = StringField('theme_name', validators=[DataRequired(message="Must provide a theme name"), Length(min=3, max=25, message="Must be between 3 and 25 characters")])
+    light_squares = StringField('light_squares', validators=[DataRequired(message="Must provide a theme name"), Length(min=3, max=25, message="Must be between 3 and 25 characters")])
+    dark_squares = StringField('dark_squares', validators=[DataRequired(message="Must provide a theme name"), Length(min=3, max=25, message="Must be between 3 and 25 characters")])
+    piece_name = StringField('dark_squares', validators=[DataRequired(message="Must provide a theme name"), Length(min=3, max=25, message="Must be between 3 and 25 characters")])
+    url = URLField('url', require_tld=True, message='This url did not work')
---
+++ app/models/theme.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Theme(db.Model):
+    '''
+    This class is responsible for storing the users background, board, and piece choices
+    '''
+
+    __tablename__='themes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    theme_name = db.Column(db.String(25), nullable=False)
+    light_squares = db.Column(db.String(25), nullable=False)
+    dark_squares = db.Column(db.String(25), nullable=False)
+    piece_name = db.Column(db.String(25), nullable=False)
+    url = db.Column(db.String(500))
+
+    user = db.relationship('User', back_populates='theme')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'theme_name': self.theme_name,
+            'light_squares': self.light_squares,
+            'dark_squares': self.dark_squares,
+            'piece_name': self.piece_name,
+            'url': self.url
+        }
