882901f378544818f5a718dde5315eb1d39b63f8
David Sexton Jr <davesextonjr@gmail.com>
2023-01-05 08:44:53 -0500

    rough in get user theme and create new theme routes

--- app/api/theme_routes.py
+++ app/api/theme_routes.py
@@ -5,3 +5,20 @@ from .auth_routes import validation_errors_to_error_messages
 from flask_login import login_required, current_user
 
 theme_routes = Blueprint('themes', __name__)
+
+@theme_routes.route('')
+@login_required
+def get_user_themes():
+    '''
+    Find and return all the users saved themes.
+    '''
+    pass
+
+
+@theme_routes.route('/new', methods=['POST'])
+@login_required
+def create_new_theme():
+    '''
+    Creates a new theme
+    '''
+    pass
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .add_new_game import NewGameForm, UpdateGameForm
+from .add_edit_theme import AddEditThemeForm
