524f2cd8997feb56199d7e9d481a975d3003a7a9
David Sexton Jr <davesextonjr@gmail.com>
2023-01-07 11:55:50 -0500

    add demo user button and further styling to the splash page

--- react-app/src/components/landing-page/LandingPage.js
+++ react-app/src/components/landing-page/LandingPage.js
@@ -2,11 +2,17 @@ import React from "react"
 import './landingPage.css'
 
 export default function LandingPage() {
+
     return (
-        <div className="button-container">
-            <div className="title">Sign in or Register to Begin Playing</div>
-            <button>Sign In</button>
-            <button>Register</button>
+        <div className="splash-background">
+            <h1>Welcome to Coder's Gambit</h1>
+            <h2>You're invited to experience the first step in a developer's journey to make a fully customizable chess experience</h2>
+            <div className="button-container">
+                <div className="title">Sign in or Register to Begin Playing</div>
+                <button>Sign In</button>
+                <button>Register</button>
+                <button>Demo User</button>
+            </div>
         </div>
     )
 }
