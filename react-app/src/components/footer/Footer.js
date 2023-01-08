7d09600baaba6a0bd4690c93b570c3543ed81a66
David Sexton Jr <davesextonjr@gmail.com>
2023-01-08 05:16:45 -0500

    add the footer

--- react-app/src/components/footer/Footer.js
+++ react-app/src/components/footer/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
     return (
         <div className="footer">
             <div> Thanks for checking out the site! Come back soon to see the continued progress.</div>
-            <div> If you'd like to find out more about the developer or how to contact him, checkout these links:</div>
-            <div id="dave-links">
-                    <a href="https://github.com/davesextonjr" target="_blank">github</a>
-                    <a href={"https://www.linkedin.com/in/dave-sexton-jr/"} target="_blank" id="dev-names">linkedin</a>
+            <div> If you'd like to find out more about the developer or how to contact him, click below:</div>
+            <div id="dave-links" className="link-container">
+                    <a href="https://github.com/davesextonjr" target="_blank">GitHub</a>
+                    <a href={"https://www.linkedin.com/in/dave-sexton-jr/"} target="_blank" id="dev-names">LinkedIn</a>
             </div>
         </div>
     )
